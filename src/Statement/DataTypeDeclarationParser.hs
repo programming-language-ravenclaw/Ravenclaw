e690386d5bb46e8347e9e1c31e227400754498ff
LuchoVera1 <luis.espinoza.13875568@usalesiana.edu.bo>
2024-06-24 19:04:57 -0400

    DataHandlerError

--- src/Statement/DataTypeDeclarationParser.hs
+++ src/Statement/DataTypeDeclarationParser.hs
@@ -33,20 +33,21 @@ dataTypeDeclarationParser = try (DataTypeDeclarationInt <$> parserDataTypeDeclar
                         <|> try (DataTypeDeclarationString <$> parserDataTypeDeclarationString)
                         <|> try (DataTypeDeclarationList <$> parserDataTypeDeclarationList)
 
+
 dataTypeIntParser :: Parser DataTypeInt
-dataTypeIntParser = DataInt <$> string "int"
+dataTypeIntParser = DataInt <$> string "int" <?> "int keyword"
 
 dataTypeFloatParser :: Parser DataTypeFloat
-dataTypeFloatParser = DataFloat <$> string "float"
+dataTypeFloatParser = DataFloat <$> string "float" <?> "float keyword"
 
 dataTypeBoolParser :: Parser DataTypeBool
-dataTypeBoolParser = DataBool <$> string "bool"
+dataTypeBoolParser = DataBool <$> string "bool" <?> "bool keyword"
 
 dataTypeStringParser :: Parser DataTypeString
-dataTypeStringParser = DataString <$> string "str"
+dataTypeStringParser = DataString <$> string "str" <?> "str keyword"
 
 dataTypeListParser :: Parser DataTypeList
-dataTypeListParser = DataList <$> string "list"
+dataTypeListParser = DataList <$> string "list" <?> "list keyword"
 
 dataType :: Parser DataType
 dataType = choice
@@ -56,61 +57,90 @@ dataType = choice
      , StrType <$> dataTypeStringParser
      , ListType <$> dataTypeListParser
     ]
+    <?> "data type"
 
 parserDataTypeDeclarationInt :: Parser DataTypeDeclarationInt
-parserDataTypeDeclarationInt = choice [try parserDataTypeDeclarationIntArith, try parserDataTypeDeclarationIntLit]
+parserDataTypeDeclarationInt = choice 
+    [ try parserDataTypeDeclarationIntArith <?> "an arithmetic int"
+    , try parserDataTypeDeclarationIntLit <?> "int literal declaration"
+    ]
+    <?> "int data type declaration"
 
 parserDataTypeDeclarationIntArith :: Parser DataTypeDeclarationInt
-parserDataTypeDeclarationIntArith = DataTypeDecIntArith
+parserDataTypeDeclarationIntArith = 
+    DataTypeDecIntArith
         <$> (spaces *> dataTypeIntParser <* spaces)
         <*> (spaces *> identifier <* spaces)
         <*> option [] (spaces *> char '=' *> spaces *> many intArithmetic' <* spaces)
+    <?> "int arithmetic declaration"
 
 parserDataTypeDeclarationIntLit :: Parser DataTypeDeclarationInt
-parserDataTypeDeclarationIntLit = DataTypeDecIntLit
+parserDataTypeDeclarationIntLit = 
+    DataTypeDecIntLit
         <$> (spaces *> dataTypeIntParser <* spaces)
         <*> (spaces *> identifier <* spaces)
         <*> option [] (spaces *> char '=' *> spaces *> many integerLiteral <* spaces)
+    <?> "int literal declaration"
 
 parserDataTypeDeclarationFloat :: Parser DataTypeDeclarationFloat
-parserDataTypeDeclarationFloat = choice [try parserDataTypeDeclarationFloatArith, try parserDataTypeDeclarationFloatLit]
+parserDataTypeDeclarationFloat = choice 
+    [ try parserDataTypeDeclarationFloatArith <?> "an arithmetic float"
+    , try parserDataTypeDeclarationFloatLit <?> "float literal declaration"
+    ]
+    <?> "float data type declaration"
 
 parserDataTypeDeclarationFloatArith :: Parser DataTypeDeclarationFloat
-parserDataTypeDeclarationFloatArith = DataTypeDecFloatArith
+parserDataTypeDeclarationFloatArith = 
+    DataTypeDecFloatArith
         <$> (spaces *> dataTypeFloatParser <* spaces)
         <*> (spaces *> identifier <* spaces)
-        <*> option [] (spaces *> char '=' *> spaces *> many  floatArithmetic' <* spaces)
+        <*> option [] (spaces *> char '=' *> spaces *> many floatArithmetic' <* spaces)
+    <?> "float arithmetic declaration"
 
 parserDataTypeDeclarationFloatLit :: Parser DataTypeDeclarationFloat
-parserDataTypeDeclarationFloatLit = DataTypeDecFloatLit
+parserDataTypeDeclarationFloatLit = 
+    DataTypeDecFloatLit
         <$> (spaces *> dataTypeFloatParser <* spaces)
         <*> (spaces *> identifier <* spaces)
-        <*> option [] (spaces *> char '=' *> spaces *> many  floatLiteral <* spaces)
+        <*> option [] (spaces *> char '=' *> spaces *> many floatLiteral <* spaces)
+    <?> "float literal declaration"
 
 parserDataTypeDeclarationBool :: Parser DataTypeDeclarationBool
-parserDataTypeDeclarationBool =
+parserDataTypeDeclarationBool = 
     DataTypeDecBool
         <$> (spaces *> dataTypeBoolParser <* spaces)
         <*> (spaces *> identifier <* spaces)
         <*> option [] (spaces *> char '=' *> spaces *> many booleanExpression <* spaces)
+    <?> "bool data type declaration"
 
 parserDataTypeDeclarationString :: Parser DataTypeDeclarationString
-parserDataTypeDeclarationString = choice [try parserDataTypeDeclarationStringArith, try parserDataTypeDeclarationStringLit]
+parserDataTypeDeclarationString = choice 
+    [ try parserDataTypeDeclarationStringArith
+    , try parserDataTypeDeclarationStringLit
+    ]
+    <?> "string data type declaration"
 
 parserDataTypeDeclarationStringArith :: Parser DataTypeDeclarationString
-parserDataTypeDeclarationStringArith = DataTypeDecStringArith
+parserDataTypeDeclarationStringArith = 
+    DataTypeDecStringArith
         <$> (spaces *> dataTypeStringParser <* spaces)
         <*> (spaces *> identifier <* spaces)
-        <*> option [] (spaces *> char '=' *> spaces *> many  stringArithmetic' <* spaces)
+        <*>option [] (spaces *> char '=' *> spaces *> many stringArithmetic' <* spaces)
+    <?> "string arithmetic declaration"
 
 parserDataTypeDeclarationStringLit :: Parser DataTypeDeclarationString
-parserDataTypeDeclarationStringLit = DataTypeDecStringLit
+parserDataTypeDeclarationStringLit = 
+    DataTypeDecStringLit
         <$> (spaces *> dataTypeStringParser <* spaces)
         <*> (spaces *> identifier <* spaces)
         <*> option [] (spaces *> char '=' *> spaces *> many stringLiteral <* spaces)
+    <?> "string literal declaration"
 
 parserDataTypeDeclarationList :: Parser DataTypeDeclarationList
-parserDataTypeDeclarationList  = DataTypeDecList
-    <$> (spaces *> dataTypeListParser <* spaces)
-    <*> (spaces *> identifier <* spaces)
-    <*> option [] (spaces *> char '=' *> spaces *> many listExpression <* spaces)
+parserDataTypeDeclarationList = 
+    DataTypeDecList
+        <$> (spaces *> dataTypeListParser <* spaces)
+        <*> (spaces *> identifier <* spaces)
+        <*> option [] (spaces *> char '=' *> spaces *> many listExpression <* spaces)
+
+    <?> "list data type declaration"
