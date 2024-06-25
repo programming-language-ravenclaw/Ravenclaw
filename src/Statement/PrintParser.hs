68f68a13da22c328a8257f322c7e5078d06f440f
ronaldo <deluxcero@gmail.com>
2024-06-25 01:04:51 -0400

    handlerErrorPrinter

--- src/Statement/PrintParser.hs
+++ src/Statement/PrintParser.hs
@@ -1,9 +1,10 @@
 module Statement.PrintParser where
 
+import Text.Parsec
 import Text.Parsec.Text (Parser)
 import AST.AST
 import Utils.ParserUtils
 import Expression.ExpressionParser
 
 printer :: Parser Printer
-printer = Print <$> (reserved "print" *> parens expression)
+printer = Print <$> (reserved "print" *> parens expression) <?> "Expected a print statement"
