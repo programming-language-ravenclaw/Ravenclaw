8d99868313a005f3daaa85e183cf8642c6cbf424
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2024-06-24 23:27:55 -0400

    Fixing merge conflicts 2 version

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,22 +4,16 @@ import Test.Hspec
 import Comment.CommentParserTest
 import Literal.LiteralParserTest
 import Statement.LoopStatement.LoopStatementTest
-<<<<<<< HEAD
 import Methods.MethodCallParserTest
 
-=======
 import Statement.ConditionalStatement.ConditionalStatementTest
 import Methods.MethodsParserTest
->>>>>>> 5d1c956b21e5dd9d1c6414cc9795aefff20a53c9
 
 main :: IO ()
 main = hspec $ do
   testParse
   testParseLiteral
   testParseLoop
-<<<<<<< HEAD
   testMethodCallParser
-=======
   testParseConditional
   testsMethodsParser
->>>>>>> 5d1c956b21e5dd9d1c6414cc9795aefff20a53c9
