20708f4899eb6ef4cfe6691ac550797d361256cc
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2024-06-24 22:41:52 -0400

    Adding documentation

--- src/Expression/ExpressionParser.hs
+++ src/Expression/ExpressionParser.hs
@@ -13,7 +13,17 @@ import Expression.ArithmeticExpressionParser
 import Utils.ParserUtils (whitespace)
 import Methods.NameMethodParser (nameMethodParser)
 
-
+-- | Parser for an expression that can be of various types.
+--
+--   This function attempts to parse an expression as:
+--
+--   * An arithmetic expression
+--   * A boolean expression
+--   * A literal expression
+--   * A list of expressions
+--   * A method call expression
+--
+--   Returns: Parsed 'Expression'.
 expression :: Parser Expression
 expression = try (ArithmeticExpr <$> arithmeticExpression) 
             <|> (BooleanExpr <$> booleanExpression) 
@@ -21,19 +31,37 @@ expression = try (ArithmeticExpr <$> arithmeticExpression)
             <|> (ListExpression <$> listExpression)
             <|> (MethodCallExpr <$> methodCallParser)
 
+-- | Parser for a list expression enclosed in square brackets.
+--
+--   Parses expressions separated by commas inside square brackets.
+--
+--   Returns: Parsed 'ListExpression'.
 listExpression :: Parser ListExpression
 listExpression = ListExpr <$> (spaces *> char '[' *> spaces *> expression `sepBy` (spaces *> char ',' <* spaces) <* spaces <* char ']' <* spaces)
 
-
+-- | Parser for the name of a method.
+--
+--   Returns: Parsed 'NameMethod'.
 nameParser :: Parser NameMethod
 nameParser = nameMethodParser
 
+-- | Parser for an expression argument.
+--
+--   Returns: Parsed 'Expression'.
 argumentParser :: Parser Expression
 argumentParser = whitespace *> expression <* whitespace
 
+-- | Parser for a list of arguments separated by commas and enclosed in parentheses.
+--
+--   Returns: List of parsed 'Expression's.
 argumentsParser :: Parser [Expression]
 argumentsParser = whitespace *> char '(' *> whitespace *> argumentParser `sepBy` (char ',' *> whitespace) <* char ')' <* whitespace
 
+-- | Parser for a method call.
+--
+--   Parses a method call consisting of a method name followed by a list of arguments.
+--
+--   Returns: Parsed 'MethodCall'.
 methodCallParser :: Parser MethodCall
 methodCallParser = do
     NameMethod name <- nameParser <?> "method name"
--- src/Methods/NameMethodParser.hs
+++ src/Methods/NameMethodParser.hs
@@ -4,5 +4,10 @@ import Text.Parsec.Text (Parser)
 import AST.AST
 import Literals.LiteralParser
 
+-- | Parser for a method name.
+--
+--   Parses an 'Identifier' and constructs a 'NameMethod'.
+--
+--   Returns: Parsed 'NameMethod'.
 nameMethodParser :: Parser NameMethod
 nameMethodParser = NameMethod <$> identifier
